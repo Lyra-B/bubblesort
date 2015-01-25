5d351efaf8c2ffdf61c70806ed7dc5360b3cc9d9
Lyra-B <glykeriapeppa@gmail.com>
2015-01-25 00:01:26 +0000

    Completed Primes

--- my_amazing_array.rb
+++ my_amazing_array.rb
@@ -5,18 +5,36 @@ class Array
   # numeric)
   #
   def sum
-    # puts self.join(",")
+    self.inject(:+)
   end
 
   # Returns the prime numbers in this array
   def primes
-    # puts self.join(",")
+    self.select do |n|
+      if n == 2 || n == 3 || n == 5 || n==7
+        n
+      elsif n >= 10
+         n if n%2 != 0 && n%3 != 0 && n%5 != 0 && n%7 != 0
+      end
+    end
   end
 
   # Examine each pair in myself and swap them until all my
   # elements are in order
   #
-  def bubble_sort
-  	# puts self.join(",")
-  end
+   def bubble_sort
+    
+  # 	# puts self.join(",")
+  #   swaps = 0
+  #   i=0
+  #   while i <= self.length-1
+  #     puts self[i],self[i+1]
+  #     i+=1
+  #     if self[i] > self[i+1]
+
+      
+
+  #   end
+
+   end
 end
