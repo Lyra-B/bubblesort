9e22471c048637ee0247c42c7826db0ccdea3873
Lyra-B <glykeriapeppa@gmail.com>
2015-01-25 23:00:27 +0000

    Completed Bubblesort

--- my_amazing_array.rb
+++ my_amazing_array.rb
@@ -1,14 +1,10 @@
 require 'pry'
 
 class Array
-  # Calculate the total sum of all the elements in this array (assuming they are
-  # numeric)
-  #
   def sum
     self.inject(:+)
   end
 
-  # Returns the prime numbers in this array
   def primes
     self.select do |n|
       if n == 2 || n == 3 || n == 5 || n==7
@@ -19,52 +15,14 @@ class Array
     end
   end
 
-  # Examine each pair in myself and swap them until all my
-  # elements are in order
-  #
   def bubble_sort
-    
+    (self.length).times do 
       for i in 0...self.length 
         if self[i].to_i > self[i+1].to_i 
           self.insert(i, self.delete_at(i+1)) 
         end 
       end 
-          # if i = self.length - 1 
-          #   self.compact 
-          # end 
-
-          # # puts self.join(",") 
-          # swaps = 0 
-          # i=0 
-          #  while i <= self.length-1 
-          #    puts self[i],self[i+1] 
-          #   i+=1 
-          #   if self[i] > self[i+1] 
-          #    end 
-          #   end 
-          # end
-
-
-
-
-        #   self.each do |i|
-        #     # def swap
-        #     # end
-        #     if self[i].to_i < self[i+1].to_i
-        #       self.insert(i, self.delete_at(i-1))
-        #       self[i] = self[i+1]
-        #     end
-        #   end
-
-
-        # # 	# puts self.join(",")
-        # #   swaps = 0
-        # #   i=0
-        # #   while i <= self.length-1
-        # #     puts self[i],self[i+1]
-        # #     i+=1
-        # #     if self[i] > self[i+1]
-    
+    end 
     self.compact
   end
 end
