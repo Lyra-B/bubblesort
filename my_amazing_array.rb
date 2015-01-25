8d5f2cb28be3bf1d0bedb1be74e9c94e35f6af0b
Lyra-B <glykeriapeppa@gmail.com>
2015-01-25 00:27:52 +0000

    Bubblesort First Attempt

--- my_amazing_array.rb
+++ my_amazing_array.rb
@@ -22,19 +22,55 @@ class Array
   # Examine each pair in myself and swap them until all my
   # elements are in order
   #
-   def bubble_sort
-    
-  # 	# puts self.join(",")
-  #   swaps = 0
-  #   i=0
-  #   while i <= self.length-1
-  #     puts self[i],self[i+1]
-  #     i+=1
-  #     if self[i] > self[i+1]
+  def bubble_sort
+    i=0 
+    self.map do |k| 
+      def swap 
+      end 
+      for i in 0...self.length
+        if self[i].to_i > self[i+1].to_i 
+        self.insert(i, self.delete_at(i+1)) 
+          # elsif self[i].to_i <=self[i+1].to_i 
+          #   next
+          #   self[i] = self[i+1] 
+        end 
+          # if i = self.length - 1 
+          #   self.compact 
+          # end 
 
-      
+          # # puts self.join(",") 
+          # swaps = 0 
+          # i=0 
+          #  while i <= self.length-1 
+          #    puts self[i],self[i+1] 
+          #   i+=1 
+          #   if self[i] > self[i+1] 
+          #    end 
+          #   end 
+          # end
 
-  #   end
 
-   end
+
+
+        #   self.each do |i|
+        #     # def swap
+        #     # end
+        #     if self[i].to_i < self[i+1].to_i
+        #       self.insert(i, self.delete_at(i-1))
+        #       self[i] = self[i+1]
+        #     end
+        #   end
+
+
+        # # 	# puts self.join(",")
+        # #   swaps = 0
+        # #   i=0
+        # #   while i <= self.length-1
+        # #     puts self[i],self[i+1]
+        # #     i+=1
+        # #     if self[i] > self[i+1]
+      end 
+    end 
+    self
+  end
 end
